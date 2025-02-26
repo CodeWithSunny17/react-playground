3bfa89cdf7d0e35ee61f2db7d3cc810c4e532dc3
Sunny Kumar <sunny274.kr@gmail.com>
2025-02-26 23:26:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center">Hello</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sample.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function sample() {
+  return <div>Hi, I dont want to loose my commit streak, Thanks!</div>;
+}
